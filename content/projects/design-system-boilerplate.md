6baa14728c5b4f35942264e277f3179334441cac
Brian Ryall <brian@brianryall.xyz>
2023-02-02 09:59:28 -0500

    fix: add my info

--- content/projects/design-system-boilerplate.md
+++ content/projects/design-system-boilerplate.md
@@ -3,4 +3,5 @@ title: A design system boilerplate with multiple themes
 description: It's a beautiful world out there.
 date: 2022-02-13
 url: https://design-system-boilerplate.netlify.app
+draft: true
 ---
--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Captain Awesome';
-export const SITE_DESCRIPTION = 'Welcome to my blog! I write about modern web development.';
-export const TWITTER_HANDLE = '@yourtwitterhandle';
-export const MY_NAME = 'Captain Awesome';
+export const SITE_TITLE = 'Brian Ryall';
+export const SITE_DESCRIPTION =
+	'Welcome to my blog! I write about various topics including development, finances, cubing, and more';
+export const TWITTER_HANDLE = '@polarmutex';
+export const MY_NAME = 'Brian Ryall';
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
