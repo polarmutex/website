593665ce8bd83c7985f5863a694d5cb2ff8e088e
Brian Ryall <brian@brianryall.xyz>
2023-02-02 09:40:52 -0500

    feat: flexdinesh-blogster template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,30 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+
+const SERVER_PORT = 3000;
+// the url to access your blog during local development
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+// the url to access your blog after deploying it somewhere (Eg. Netlify)
+const LIVE_URL = 'https://brianryall.xyz';
+// this is the astro command your npm script runs
+const SCRIPT = process.env.npm_lifecycle_script || '';
+const isBuild = SCRIPT.includes('astro build');
+let BASE_URL = LOCALHOST_URL;
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+	BASE_URL = LIVE_URL;
+}
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	server: { port: SERVER_PORT },
+	site: BASE_URL,
+	integrations: [
+		sitemap(),
+		tailwind({
+			config: { applyBaseStyles: false },
+		}),
+	],
 });
